88f94bab0c1d71f3b52e05335ea17287f5c48319
Мария Куреннова <mariika5@MacBook-Pro-Maria-2.local>
2018-06-23 17:28:39 +0300

    add active admin and clients

--- db/migrate/20180623112320_create_clients.rb
+++ db/migrate/20180623112320_create_clients.rb
@@ -2,7 +2,7 @@ class CreateClients < ActiveRecord::Migration[5.1]
   def change
     create_table :clients do |t|
       t.string :name
-      t.string :emale
+      t.string :email
       t.string :phone
 
       t.timestamps
