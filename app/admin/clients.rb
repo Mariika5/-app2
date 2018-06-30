6c993ad466825bf8fad19ec6226fed060e6e432e
mariika5 <mariika5@mail.ru>
2018-06-30 14:35:43 +0300

    add last_name and change admin coloms

--- app/admin/clients.rb
+++ app/admin/clients.rb
@@ -1,6 +1,16 @@
 ActiveAdmin.register Client do
 
+   permit_params :name, :last_name, :email, :phone
 
-permit_params :name, :email, :phone
+index do
+   column :name
+   column :last_name
+   column :phone
+   column :email
+   column :created_at
+   actions
+ end
+
+
 
 end
--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,8 @@
 class Client < ApplicationRecord
+	 validates :name, :last_name, presence: true, length: { minimum: 2 }
+     validates :email, presence: true
+     validates :phone, presence: true
+     validates :phone, format: { with: /\A(\+7|8)[\d]{10}\z/ }
+     validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i  } 
 end
+
