26bc607639d711058e0112c11da3cf66b69149f8
mariika5 <mariika5@mail.ru>
2018-06-30 15:20:56 +0300

    full_name

--- app/admin/clients.rb
+++ app/admin/clients.rb
@@ -3,8 +3,7 @@ ActiveAdmin.register Client do
    permit_params :name, :last_name, :email, :phone
 
 index do
-   column :name
-   column :last_name
+   column :full_name
    column :phone
    column :email
    column :created_at
--- app/models/client.rb
+++ app/models/client.rb
@@ -4,5 +4,23 @@ class Client < ApplicationRecord
      validates :phone, presence: true
      validates :phone, format: { with: /\A(\+7|8)[\d]{10}\z/ }
      validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i  } 
+
+
+     def full_name
+     	if name && last_name 
+     		"#{name} #{last_name}"
+     	elsif name.empty?
+     	    last_name
+     	elsif last_name.empty?
+     		name
+     	else 
+     		""
+     	end
+     end
 end
 
+
+
+# - Если name и last_name не nil и не пустая строка - то складываем name, last_name, между ними пробел
+# - Если name или last_name nil или пустая строка - то берем только не пустое поле
+# - Если и name и last_name пустая строка или nil - возвращаем пустую строку
